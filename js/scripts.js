44149b3480c0b333bcd7cd0bb8c0e013a3436865
Lucas <gomez-lucas@live.com.ar>
2019-03-09 21:02:51 -0300

    Version finalizada /javascript sin funcionar/
    Version

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+ 
+ $(".mainheader__logo__oculto").click(function()
+{ 
+$(".mainheader__menu").fadeToggle();
+
+});
+
+ /*
+$(".mainheader__menu li").click(function()
+{ 
+$(".mainheader__menu").hide();
+ 
+}) ;
+ 
+ */
+ 
