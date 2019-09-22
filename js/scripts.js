9f32cbaa55d1c78b114e535e068c9d547f1af171
Lucas <gomez-lucas@live.com.ar>
2019-09-22 18:47:36 -0300

    Lodo modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,19 @@
 let buttonX = document.querySelector(".buttonX")
 let mainX = document.querySelector(".mainheader__buttonX")
-  
+let logo = document.querySelector(".mainheader__logo")
 
-buttonX.addEventListener("click", function(){
-     let value = mainX.classList.contains("mainheader__buttonX_show")
-     if(value){
+
+buttonX.addEventListener("click", function () {
+    let value = mainX.classList.contains("mainheader__buttonX_show")
+    if (value) {
         mainX.classList.remove("mainheader__buttonX_show")
         buttonX.classList.remove("change")
-    }else{
+    } else {
         mainX.classList.add("mainheader__buttonX_show")
         buttonX.classList.add("change")
     }
-    
+
 })
+
+
+logo.animate([{opacity:'0.0'},{opacity:'1.0'}],{duration:2000,fill:'forwards'});
