cdbee12ac4f2282dbad4303c1ac7003e365a7b9d
Lucas <gomez-lucas@live.com.ar>
2019-09-22 23:33:58 -0300

    navBar Click added

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,24 @@ buttonX.addEventListener("click", function () {
         mainX.classList.add("mainheader__buttonX_show")
         buttonX.classList.add("change")
     }
+})
+
+window.addEventListener("click", function (event) {
+    let value = mainX.classList.contains("mainheader__buttonX_show");
+    console.log(buttonX.children[0])
+    
+    if (mainX != event.target && 
+        mainX != event.target.parentNode && 
+        event.target != buttonX &&
+        event.target != buttonX.children[0] &&
+        event.target != buttonX.children[1] &&
+        event.target != buttonX.children[2] 
+         ) {
+        mainX.classList.remove("mainheader__buttonX_show")
+        buttonX.classList.remove("change")
+
+    }
 
 })
 
-
-logo.animate([{opacity:'0.0'},{opacity:'1.0'}],{duration:3000,fill:'forwards'});
+logo.animate([{ opacity: '0.0' }, { opacity: '1.0' }], { duration: 3000, fill: 'forwards' });
